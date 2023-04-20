565550a835e4e4d873b5a3e83b1440f68b6607f5
fbritojunior <fbritojnr@gmail.com>
2023-04-20 02:36:11 -0300

    Atualização do código

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -13,10 +13,23 @@ class Dashboard extends Component {
 		super(props);
 		//this.textInput = React.createRef();
 		this.svgRef = createRef();
+
+		this.state = {
+			researchVar : null,
+			researchYear: null,
+			dataCounties: null 
+		};
 	}
 
 	componentDidMount() {
-		this.drawChart();
+		const urlApi = 'https://servicodados.ibge.gov.br/api/v3/agregados/1301/periodos/2010/variaveis/615|616?localidades=N3[23]|N6[N3[23]]'
+    	fetch(urlApi)
+		.then((response) => response.json())
+		.then((json) =>  
+			this.setState({ dataCounties: json[0].resultados[0].series, researchVar:json[0].variavel }, () => this.drawChart())
+    	);
+
+		//this.drawChart();
 	}
 
 	drawChart() {
@@ -27,19 +40,37 @@ class Dashboard extends Component {
 			.attr("preserveAspectRatio", "xMidYMid meet")
 			.style("cursor", "auto");
 		svg.attr("width", 700).attr("height", 700);
-
+		
 		var mapa = svg.append("g");//.attr("class", "mapa").attr("id", "mapaid");
 
 		var projection = d3.geoMercator().scale(5800).rotate([0, 0]).center([-38, -4.50]);
 
 		var path = d3.geoPath().projection(projection);
 
-		var values = this.props.data
+		var values = this.props.data;
+		var dataValuesIbge = this.state.dataCounties;
+		const researchYear = Object.keys(dataValuesIbge[0].serie);
+		let dataValues = [], dataName = [], groupData = [];
+
+		for (let i=1; i<dataValuesIbge.length; i++){
+			dataName = (dataValuesIbge[i].localidade.nome.split('-')[0].trim());
+			dataValues = (parseFloat(dataValuesIbge[i].serie[researchYear]));
+			groupData.push({name: dataName,value: dataValues});
+		}
 
 		var features = topojson.feature(values, values.objects.municipalities).features;
 
-		var tooltip = d3.select('.tooltip-area')
-			.style('opacity', 0);
+		//var tooltip = d3.select('.tooltip-area').style('opacity', 0);
+
+		var Tooltip = d3.select("#tooltipId")
+			.append("div")
+			.style("opacity", 0)
+			.attr("class", "tooltip")
+			.style("background-color", "white")
+			.style("border", "solid")
+			.style("border-width", "2px")
+			.style("border-radius", "5px")
+			.style("padding", "5px")
 
 		function mouseOver(event, d) {
 
@@ -47,29 +78,50 @@ class Dashboard extends Component {
 			var currentColor = currentPath.style('fill');
 
 			currentPath//.transition()
-				.style("stroke", "gray").style("stroke-width", 1).style("opacity", 0.5)
-				.style('fill', 'black')
+				.style("stroke", "white").style("stroke-width", 2).style("opacity", 0.25)
+				//.style('fill', 'white')
 				.style("cursor", "pointer");
 
-			tooltip.style("opacity", 1);
+			//tooltip.style("opacity", 1);
+			Tooltip.style("opacity", 1);
+
 		};
 
 		const mouseMove = (event, d) => {
-			const text = d3.select('.tooltip-area__text');
-			text.text(`${d.properties.name.toString().toLowerCase()} `);
-			const [x, y] = d3.pointer(event);
+			
+			const propName = d.properties.name.toLocaleLowerCase();
+			const filteredItem = groupData.filter(e => e.name.toLocaleLowerCase() === propName);
+			//const values = Object.values(filteredItem[0])
 
-			tooltip
-				.attr('transform', `translate(${x}, ${y})`);
+			//const text = d3.select('.tooltip-area__text');
+			//text.text(`${d.properties.name.toString().toLowerCase()} ${ba[0].val} `);	
+			//const [x, y] = d3.pointer(event);
+			//tooltip.attr('transform', `translate(${x}, ${y})`);
+
+			Tooltip
+				.html(`${propName.split(' ').map(str => str.replace(/^./, s => s.toUpperCase())).join(' ').replace(/\sD[aeiou]\s/g, ss => ss.toLocaleLowerCase())} `)
+				.style("left", (event.pageX + 20) + "px")
+				.style("top", (event.pageY) + "px");
 		};
 
-		mapa.append("g").selectAll("path").data(features).enter().append("path").attr("name", function (d) {
+		var color = d3.scaleLinear()
+		.domain([1, 200])  
+		.range(["lightblue", "steelblue"]);
+
+		var myColor = d3.scaleSequential()
+    .interpolator(d3.interpolateInferno)
+    .domain([1,100])
+
+		mapa.append("g").selectAll("path").data(features).enter().append("path")
+		.attr("name", function (d) {
 			return d.properties.name;
 		})
 			.attr("id", function (d) {
 				return d.id;
 			})
 			.attr("d", path)
+			//.attr('fill', '#e7d8ad')
+			.attr('fill', function(d,i) {  return color(i); })
 			.on("mousemove", mouseMove)
 			.on('mouseover', mouseOver //function (d) {
 				//d3.select(this)
@@ -87,10 +139,10 @@ class Dashboard extends Component {
 				d3.select(this)
 					.style("stroke", null).style("fill", null).style("stroke-width", 0.5).style('opacity', 1);
 				//d3.select('.details').style('visibility', "hidden");
-				tooltip.style('opacity', 0);
+				//tooltip.style('opacity', 0);
+				Tooltip.style("opacity", 0);
 			})
 			.on("click", function (d) { });
-
 	}
 
 	render() {
@@ -104,22 +156,19 @@ class Dashboard extends Component {
 		//)
 		return (
 			<Layout style={{ height: 870 }}>
-				<Header>header</Header>
+				<Header className="dash-header" > Dashboard para Visualização de Dados com React + D3.Js</Header>
 				<Layout>
-					<Sider>left sidebar</Sider>    
-					<Content>
-						<svg ref={this.svgRef} >	
-							<g className="tooltip-area">
-								<text className="tooltip-area__text"></text>
-							</g>
+					<Sider>sidebar</Sider>    
+					<Content className="dash-content" >
+						<div id="tooltipId" />
+						<svg ref={this.svgRef} >
 						</svg>
 					</Content>
-					 
 				</Layout>
 				<Footer>
-					<div style={{marginTop: 10}}>
-                        Source Code <a href='https://github.com/'>https://github.com/</a>;
-                        Author <a href='https://sdq.ai'>fbrito</a>;
+					<div style={{marginTop: 0}}>
+                        <p style={{marginBottom: 0}}>Desenvolvido por <a href='https://github.com/fbrito'>fbritojunior</a>.</p>
+						<p>Código fonte em <a href='https://github.com/fbrito'>https://github.com/</a></p>
                     </div>
 				</Footer>
 			</Layout>
